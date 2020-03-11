81c9c2482f33d33e34c9715626084658252d829a
Giwan <giwan.kim@gmail.com>
2020-03-11 21:10:18 +0900

    Add script to kfold split with respect to multi-label

--- src/create_folds.py
+++ src/create_folds.py
@@ -1,20 +1,24 @@
 import pandas as pd
 from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 
+
 if __name__ == '__main__':
     df = pd.read_csv('../input/train.csv')
     print(df.head())
-    df['kfold'] = -1
 
+    # Shuffle dataframe
     df = df.sample(frac=1).reset_index(drop=True)
 
-    X = df['image_id'].values
+    x = df['image_id'].values
     y = df[['grapheme_root', 'vowel_diacritic', 'consonant_diacritic']].values
+
+    # Column to indicate validation fold number
+    df['kfold'] = -1
 
     mskf = MultilabelStratifiedKFold(n_splits=5)
 
-    for fold, (train_idx, val_idx) in enumerate(mskf.split(X, y)):
-        print('TRAIN:', train_idx)
+    for fold, (trn_idx, val_idx) in enumerate(mskf.split(x, y)):
+        print("TRAIN:", trn_idx)
         print('VAL:', val_idx)
         df.loc[val_idx, 'kfold'] = fold
 
