730d70628018e44e025d1ee9992bb84931c16e3d
Giwan <giwan.kim@gmail.com>
2020-03-16 13:00:21 +0900

    Add SEResNext50 model

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -2,4 +2,5 @@ import models
 
 MODEL_DISPATCHER = {
     'resnet34': models.ResNet34,
+    'seresnext50': models.SEResNext50,
 }
--- src/models.py
+++ src/models.py
@@ -24,3 +24,29 @@ class ResNet34(nn.Module):
         out1 = self.l1(x)
         out2 = self.l2(x)
         return out0, out1, out2
+
+
+class SEResNext50(nn.Module):
+    def __init__(self, pretrained):
+        super(SEResNext50, self).__init__()
+        if pretrained is True:
+            self.model = pretrainedmodels.__dict__[
+                'se_resnext50_32x4d'](pretrained='imagenet')
+        else:
+            self.model = pretrainedm
+
+        self.l0 = nn.Linear(2048, 168)
+        self.l1 = nn.Linear(2048, 11)
+        self.l2 = nn.Linear(2048, 7)
+
+    def forward(self, x):
+        batch_size = len(x)
+
+        x = self.model.features(x)
+
+        x = F.adaptive_avg_pool2d(x, 1).reshape(batch_size, -1)
+
+        l0 = self.l0(x)
+        l1 = self.l1(x)
+        l2 = self.l2(x)
+        return l0, l1, l2
